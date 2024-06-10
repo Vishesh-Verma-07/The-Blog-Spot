8fd6c5a6e802bfbf58d70dfd3db306bdb437ce03
Vishesh Verma <visheshverma250703@gmail.com>
2024-06-10 19:38:36 +0530

    add components for logo, container, footer, button, header, input, logoutbtn

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,15 @@
+import { Container } from "postcss";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
 
 
 export{
     Header, 
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
